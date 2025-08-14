912a5da8c1ba952826181239e767a86774096f56
mejin <wls4482@gmail.com>
2025-08-15 01:45:43 +0900

    feat: add 이름 input

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -14,6 +14,26 @@ const LoginPage = () => {
     "
     >
       <PageHeader title="사용자 정보 입력" />
+      <div className="flex flex-col items-stretch bg-white">
+        {/* 이름 */}
+        <div className="flex flex-col items-start justify-start gap-1">
+          <div className="flex flex-row gap-2 overflow-hidden">
+            <p className="text-xs font-semibold text-black">이름</p>
+          </div>
+          <div
+            className="
+              flex flex-row                 
+              w-[361px] h-[44px]             
+              items-center                   
+              px-4 py-3                      
+              rounded-lg                     
+              bg-neutral-100                 
+              border border-neutral-300      
+              overflow-hidden                
+            "
+          ></div>
+        </div>
+      </div>
     </div>
   );
 };
