7cce12f85ff8fb78f734923a29ed070306459818
mejin <wls4482@gmail.com>
2025-08-15 00:59:30 +0900

    feat(LoginPage): header 구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import PageHeader from "../components/PageHeader";
+
 const LoginPage = () => {
-  return <div className="flex flex-col gap-3 p-4"></div>;
+  return (
+    <div
+      className="
+    flex flex-col           
+    w-full min-h-screen     
+    items-center            
+    gap-5              
+    bg-white                
+    overflow-hidden         
+    "
+    >
+      <PageHeader title="사용자 정보 입력" />
+    </div>
+  );
 };
 export default LoginPage;
